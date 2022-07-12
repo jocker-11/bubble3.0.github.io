4d531e7615a252ce7cabc62f7dfcad0932fdde44
jocker-11 <76844689+jocker-11@users.noreply.github.com>
2022-07-12 15:58:16 +0800

    haha

---
+++ bubble/ans.js
@@ -0,0 +1,15 @@
+document.getElementById("submit").addEventListener("click", test());
+
+function test(){
+    var x = getElementById("CommentText").value;
+    var ans_1= "#inclde<stdio.h>";
+
+    if (x == ans_1){
+        alert("答對了");
+    }
+    else{
+        alert("再想想吧");
+    }
+
+}
+
